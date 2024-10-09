df9e3b36b4b2875ed264b6650c845994d2c73b77
Diego Oliveros <doliveros@correo.unimet.edu.ve>
2024-10-08 22:16:11 -0400

    :rocket:

---
+++ Prueba - Copy - Copy/src/MarcaTrabajadores/Trabajador.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MarcaTrabajadores;
+
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import main.ProductoresComputadoras;
+
+
+
+
+/**
+ *
+ * @author diego
+ */
+public class Trabajador extends Thread {
+    
+}
